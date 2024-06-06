458a3010ee0509ad0268d9e14fefd4db705bd128
Jeffrey Ebrado <145235529+JeffreyEbrado@users.noreply.github.com>
2024-06-06 12:21:54 -1100

    Update 404.php

--- resources/views/error/404.php
+++ resources/views/error/404.php
@@ -1,11 +1,4 @@
-<?php
-$title = 'Page Not Found';
-ob_start();
-?>
-
 <h1>404 - Page Not Found</h1>
 <p>The page you are looking for could not be found.</p>
 
-<?php
-$content = ob_get_clean();
-include __DIR__ . '/../layouts/main.php';
+
