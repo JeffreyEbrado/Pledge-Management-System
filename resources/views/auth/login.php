0316257c29171f917671830cd163cd111bd6a6da
Jeffrey Ebrado <145235529+JeffreyEbrado@users.noreply.github.com>
2024-06-05 23:37:18 -1100

    Update login.php

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -1,18 +1,7 @@
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Attendance Track</title>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="/public/css/styles.css">
-</head>
-<body>
-    <div class="main_container">
-        
-        <!--1. display details-->
         <div class="display_details">
            Login
         </div>
         
-        <!--2. display inputs, their label and submit buttons-->
         <form class="input_panel center_all_in_one_direction">
             <label class="label">ID</label>
             <input type="text" name="id" class="id_textfield">
@@ -20,9 +9,8 @@
             <input type="password" name="password" class="id_textfield">
             <button type="submit" class="submit_button">Login</button>
         </form>
-        <!--3. label and register text button-->
+        
         <div class="guide_panel">
           <label>You don't have an account? <a href="?action=register">Register Here</a></label>
         </div>
-    </body>
-</html>
+
