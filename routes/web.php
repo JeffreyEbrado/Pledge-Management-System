31dc6b9ae053e2a91ff4ec1d70e13034f17bc5e5
Jeffrey Ebrado <145235529+JeffreyEbrado@users.noreply.github.com>
2024-06-07 17:44:52 -1100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,5 @@ $routes = [
     'login' => 'resources/views/auth/login_form.php',
     'register' => 'resources/views/auth/register_form.php',
     'system' => 'resources/views/auth/system.php'
-   // 'register_process' => 'app/model/access db/register_process.php'
 ];
 ?>
